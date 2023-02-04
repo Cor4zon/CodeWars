46fde10306a7f2404b1149dca326416fe3a04024
Victor Fedorov <vpfedorov25@gmail.com>
2023-02-04 10:04:56 +0400

    56 task

---
+++ 7kata/ConvertHashToArray.js
@@ -0,0 +1,14 @@
+function convertHashToArray(hash){
+  const resultArray = [];
+  for (let key in hash) {
+    resultArray.push([key, hash[key]]);
+  }
+  resultArray.sort((a, b) => {
+    if (a[0] > b[0]) {
+      return 1;
+    } else if (a[0] < b[0]) {
+      return -1;
+    }
+  });
+  return resultArray;
+}
