b3a237478d877cda29a6a76dd69048f0f604df06
Victor Fedorov <vpfedorov25@gmail.com>
2023-02-15 22:41:14 +0400

    more optimal solution

--- 5kata/FirstNon-repeatingCharacter.js
+++ 5kata/FirstNon-repeatingCharacter.js
@@ -11,3 +11,26 @@ function firstNonRepeatingLetter(s) {
   return "";
 
 }
+
+
+// smarter
+function firstNonRepeatingLetter(s) {
+  const map = {};
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i].toLowerCase();
+    map[char] = (map[char] || 0) + 1;
+  }
+
+  console.log(map);
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i].toLowerCase();
+    if (map[char] === 1) {
+      return s[i];
+    }
+  }
+  return "";
+}
+
+// best
