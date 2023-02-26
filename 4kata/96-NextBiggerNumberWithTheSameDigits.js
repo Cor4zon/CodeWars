98cdcff9ae20d238da0e6163b948e53a52be3ce7
Victor Fedorov <vpfedorov25@gmail.com>
2023-02-26 11:33:25 +0400

    96 wip

--- 4kata/96-NextBiggerNumberWithTheSameDigits.js
+++ 4kata/96-NextBiggerNumberWithTheSameDigits.js
@@ -16,7 +16,17 @@ function nextBigger(n) {
   const arrayString = stringNumber.split('');
   let digit1 = arrayString[index1];
   let digit2 = arrayString[index2];
+
+  console.log(arrayString);
+
   arrayString[index1] = digit2;
   arrayString[index2] = digit1;
+  console.log(arrayString);
+//   console.log(arrayString.slice(0, index1));
+//   console.log(arrayString.slice(index1, arrayString.length + 1).sort());
   return +arrayString.join('');
 }
+
+// console.log(nextBigger(463682));
+
+console.log(nextBigger(513));
